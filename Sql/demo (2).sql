95b0f3c4e82495ba869c66167661d56868c86ee6
sanjay979 <84920091+sanjay979@users.noreply.github.com>
2023-05-30 22:30:43 +0530

    database file updated

--- Sql/demo (2).sql
+++ Sql/demo (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2023 at 07:02 PM
+-- Generation Time: May 30, 2023 at 06:59 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -78,7 +78,8 @@ INSERT INTO `faculty_details` (`s_id`, `name`, `department`, `dob`) VALUES
 ('01fbt102', 'John', 'botany', '1967-05-10'),
 ('01fcs101', 'dpj', 'computer science', '1965-02-14'),
 ('01fcs110', 'ravindran', 'computer science', '1967-05-10'),
-('01fcs111', 'Charles', 'computer science', '1967-05-10');
+('01fcs111', 'Charles', 'computer science', '1967-05-10'),
+('01fqc101', 'IQAC', 'chemistry', '1967-05-10');
 
 -- --------------------------------------------------------
 
@@ -102,6 +103,42 @@ INSERT INTO `hod_login` (`s_id`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `iqac_login`
+--
+
+CREATE TABLE `iqac_login` (
+  `s_id` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `iqac_login`
+--
+
+INSERT INTO `iqac_login` (`s_id`, `password`) VALUES
+('01fqc101', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `principal_login`
+--
+
+CREATE TABLE `principal_login` (
+  `s_id` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `principal_login`
+--
+
+INSERT INTO `principal_login` (`s_id`, `password`) VALUES
+('01sjc001', '123');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `staff_login`
 --
 
@@ -119,6 +156,66 @@ INSERT INTO `staff_login` (`s_id`, `password`, `c_password`) VALUES
 ('01fbt102', '123', '123'),
 ('01fcs101', '123', '123'),
 ('01fcs110', '123', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbldepartments`
+--
+
+CREATE TABLE `tbldepartments` (
+  `id` int(11) NOT NULL,
+  `DepartmentName` varchar(150) DEFAULT NULL,
+  `DepartmentShortName` varchar(100) NOT NULL,
+  `DepartmentCode` varchar(50) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbldepartments`
+--
+
+INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `DepartmentCode`, `CreationDate`) VALUES
+(1, 'Computer Science', 'CS', 'CS160', '2020-11-01 07:16:25'),
+(2, 'Information Technology', 'IT', 'IT807', '2020-11-01 07:19:37'),
+(3, 'Chemistry', 'CH', 'CH640', '2020-12-02 21:28:56'),
+(4, 'Physics', 'PH', 'PH9696', '2021-03-03 08:27:52'),
+(5, 'Commerce', 'CO', 'CO369', '2021-03-03 10:53:52'),
+(6, 'Economics', 'EC', 'EC123', '2021-03-03 10:54:27'),
+(7, 'English', 'ENG', 'ENG469', '2021-03-03 10:55:24'),
+(8, 'Tamil', 'TAM', 'TAM666', '2021-03-03 16:39:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `id` int(11) NOT NULL,
+  `EmpId` varchar(150) NOT NULL,
+  `FirstName` varchar(150) NOT NULL,
+  `LastName` varchar(150) NOT NULL,
+  `EmailId` varchar(150) NOT NULL,
+  `Password` varchar(150) NOT NULL,
+  `Gender` varchar(100) NOT NULL,
+  `Dob` date NOT NULL,
+  `Department` varchar(150) NOT NULL,
+  `Qualification` varchar(150) NOT NULL,
+  `Address` varchar(200) NOT NULL,
+  `City` varchar(150) NOT NULL,
+  `Country` varchar(150) NOT NULL,
+  `Phonenumber` int(10) NOT NULL,
+  `Status` int(1) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Dob`, `Department`, `Qualification`, `Address`, `City`, `Country`, `Phonenumber`, `Status`, `RegDate`) VALUES
+(1, '22pca132', 'peter', 'thamiyan', 'peter@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'MALE', '2002-07-01', 'Computer Science', 'MCA', '2-20 east street, Alambakkam', 'Trichy', 'India', 2147483647, 1, '2023-05-29 23:50:16');
 
 --
 -- Indexes for dumped tables
@@ -149,6 +246,18 @@ ALTER TABLE `staff_login`
   ADD PRIMARY KEY (`s_id`);
 
 --
+-- Indexes for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -157,6 +266,18 @@ ALTER TABLE `staff_login`
 --
 ALTER TABLE `faculty1`
   MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
