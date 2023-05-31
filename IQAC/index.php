3a391b9e845fb5d30a481f39de7e953a564f994e
sanjay979 <84920091+sanjay979@users.noreply.github.com>
2023-05-31 19:32:25 +0530

    header problem solved

--- IQAC/index.php
+++ IQAC/index.php
@@ -1,7 +1,3 @@
-<?php
-
-session_start();
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,33 +15,7 @@ session_start();
 
   <?php include 'sidebar.php';?>
     <div class="main-content">
-    <header><h2>
-             <div class="header-list">
-                <label for="nav-toggle">
-                    <span class="las la-bars"></span>
-                </label> Dashboard
-            </div>
-           
-        </h2>
-           <div class="user-wrapper">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMi1noTDjkelW0kvsZO5CgEaBM5GHrNkF9ix7Knt9Ztw&s" alt="" width="30px" height="30px">
-            <div>
-                <h4><?php
-        $id=$_SESSION['s_id'];
-        include '../database/Databasedemo.php';
-        $sql="SELECT name FROM faculty_details where s_id='$id'";
-        $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $value = $row['name'];
-                echo  $value;
-            }
-        ?></h4>
-                <small>IQAC</small>
-            </div>
-           </div>
-        </header>
+    <?php include 'header.php'?>
     <main>
         <div class="cards">
                 <div class="card-single">
