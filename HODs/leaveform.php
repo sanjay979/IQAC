b1bca69f60c20aac4b7e40554b67d0273f966457
peterthamiyan <peterthamiyan2616@gmail.com>
2023-06-19 01:13:58 -0700

    Update leaveform.php
    hod-- leaveform

--- HODs/leaveform.php
+++ HODs/leaveform.php
@@ -19,88 +19,121 @@ if ($_SESSION['s_id']) {
             @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap');
 
 
-            form .fields .input-field {
-                display: flex;
-                width: calc(100% / 2 - 15px);
-                flex-direction: column;
-                margin: 4px 0;
-            }
-
-            .input-field label {
-                font-size: 12px;
-                font-weight: 500;
-                color: #2e2e2e;
-            }
-
-            form .fields {
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                flex-wrap: wrap;
-            }
-
-            .input-field input :focus,
-            .input-field select:focus {
-                box-shadow: 0 3px 6px rgba(0, 0, 0, 0.13);
-            }
-
-            .input-field input,
-            select {
-                outline: none;
-                font-size: 14px;
-                font-weight: 400;
-                color: #333;
-                border-radius: 5px;
-                border: 1px solid #aaa;
-                padding: 0 15px;
-                height: 42px;
-                margin: 8px 0;
-            }
-
-            form button i {
-                margin: 0 6px;
-            }
-
-            form button:hover {
-                background-color: #265df2;
-            }
-
-            form button {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 45px;
-                max-width: 200px;
-                width: 100%;
-                border: none;
-                outline: none;
-                color: #fff;
-                border-radius: 5px;
-                margin: 25px 0;
-                background-color: #4070f4;
-                transition: all 0.3s linear;
-                cursor: pointer;
-            }
-
-            @media (max-width: 750px) {
-                .container form {
-                    overflow-y: scroll;
-                }
-
-                .container form::-webkit-scrollbar {
-                    display: none;
-                }
-
-                form .fields .input-field {
-                    width: calc(100% / 2 - 15px);
-                }
-            }
-
-            @media (max-width: 550px) {
-                form .fields .input-field {
-                    width: 100%;
-                }
-            }
+            body {
+    font-family: Arial, sans-serif;
+    background-color: #f2f2f2;
+    margin: 0;
+    padding: 0;
+  }
+  
+  .form_center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    height: 100vh;
+    margin-top: 0px;
+  }
+  
+  form {
+    max-width:750px;
+    margin: 0 auto;
+    background-color: #ffffff;
+    padding: 60px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add box shadow for a subtle effect */
+  }
+  
+  h1 {
+    text-align: center;
+    margin-bottom: 20px;
+    margin-top: 0;
+    color: #4070f4; /* Change heading color */
+  }
+  
+  .input-field {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  
+  .input-field label {
+    font-size: 19px;
+    font-weight: 500;
+    color: #2e2e2e;
+    width: 200px;
+    margin-right: 16px;
+  }
+  
+  .input-field input,
+  textarea,
+  select {
+    outline: none;
+    flex: 1;
+    font-size: 17px;
+    font-weight: 400;
+    color: #333;
+    border-radius: 5px;
+    border: 1px solid #aaa;
+    padding: 10px;
+    height: 42px;
+  }
+  
+  textarea {
+    resize: vertical;
+  }
+  
+  form button i {
+    margin: 0 6px;
+  }
+  
+  form button:hover {
+    background-color: #265df2;
+  }
+  
+  form button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 45px;
+    max-width: 200px;
+    width: 100%;
+    border: none;
+    outline: none;
+    color: #fff;
+    border-radius: 5px;
+    margin-top: 25px;
+    background-color: #4070f4;
+    transition: all 0.3s linear;
+    cursor: pointer;
+  }
+  
+  @media (max-width: 600px) {
+    form {
+      padding: 20px;
+    }
+  
+    .input-field {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+  
+    .input-field label {
+      width: auto;
+      margin-bottom: 8px;
+    }
+  
+    .input-field input,
+    textarea,
+    select {
+      font-size: 12px;
+    }
+  
+    form button {
+      font-size: 12px;
+    }
+  }
         </style>
     </head>
 
