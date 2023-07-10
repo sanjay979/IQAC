5c4a3b3946e76b2bb035ad12bd6097d3a6a71a7c
JoyFrancs <joyalcraz81@gmail.com>
2023-07-10 21:34:28 +0530

    Status view , Navigation to LeavForm feature-staff

--- Staff/index.php
+++ Staff/index.php
@@ -14,6 +14,8 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css">
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 
+        <link rel="stylesheet" type="text/css" href="index.css">
+
         <link rel="stylesheet" type="text/css" href="sidebar.css">
     </head>
 
@@ -22,8 +24,8 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
         <?php include 'Sidebar.php'; ?>
         <div class="main-content">
 
-            <?php 
-            include "header.php"; 
+            <?php
+            include "header.php";
             /*
             <header>
                 <h2>
@@ -55,8 +57,8 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
             </header>
             */ ?>
             <main>
-            <?php
-             $id = $_SESSION['s_id'];
+                <?php
+                $id = $_SESSION['s_id'];
                 $sql1 = "SELECT sum(ndays) AS ml from faculty1 where principal=1 and LType='ML' and id='$id'";
                 $sql2 = "SELECT sum(ndays) AS od from faculty1 where principal=1 and LType='OD' and id='$id'";
                 $sql3 = "SELECT sum(ndays) AS cl from faculty1 where principal=1 and LType='CL' and id='$id'";
@@ -66,23 +68,22 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
 
 
                 ?>
-               <div class="cards">
+                <div class="cards">
                     <div class="card-single">
                         <div>
-                            <h1><?php 
-                            if($result1){
-                                $row = mysqli_fetch_assoc($result1);
-  
-                                
-                                $ml = $row['ml'];
-                                if($ml==0){
-                                    echo '0';
-                                }else{
-                                    echo $ml;
-                                
+                            <h1><?php
+                                if ($result1) {
+                                    $row = mysqli_fetch_assoc($result1);
+
+
+                                    $ml = $row['ml'];
+                                    if ($ml == 0) {
+                                        echo '0';
+                                    } else {
+                                        echo $ml;
+                                    }
                                 }
-                            }
-                             ?></h1>
+                                ?></h1>
                             <span>No of days ML</span>
                         </div>
                         <div>
@@ -91,17 +92,17 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
                     </div>
                     <div class="card-single">
                         <div>
-                            <h1><?php 
-                            if($result2){
-                                $row = mysqli_fetch_assoc($result3);
-                                $cl = $row['cl'];
-                                if($cl==0){
-                                    echo '0';
-                                }else{
-                                    echo $cl;
+                            <h1><?php
+                                if ($result2) {
+                                    $row = mysqli_fetch_assoc($result3);
+                                    $cl = $row['cl'];
+                                    if ($cl == 0) {
+                                        echo '0';
+                                    } else {
+                                        echo $cl;
+                                    }
                                 }
-                            }
-                             ?></h1>
+                                ?></h1>
                             <span>No of days CL</span>
                         </div>
                         <div>
@@ -110,15 +111,16 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
                     </div>
                     <div class="card-single">
                         <div>
-                            <h1><?php 
-                            if($result3){
-                                $row = mysqli_fetch_assoc($result2);
-                                $od = $row['od'];
-                                if($od==0){
-                                    echo '0';
-                                }else{
-                                    echo $od;
-                                }}?></h1>
+                            <h1><?php
+                                if ($result3) {
+                                    $row = mysqli_fetch_assoc($result2);
+                                    $od = $row['od'];
+                                    if ($od == 0) {
+                                        echo '0';
+                                    } else {
+                                        echo $od;
+                                    }
+                                } ?></h1>
                             <span>No of days OD</span>
                         </div>
                         <div>
@@ -126,7 +128,90 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
                         </div>
                     </div>
                 </div>
-                <?php include 'secondform.php';?>
+
+                <?php
+
+                $sql = "SELECT * FROM faculty1 WHERE id = '" . $id . "' AND (hod = FALSE OR aqict = FALSE OR principal = FALSE OR hod = 3 OR aqict = 3 OR principal = 3 )";
+                $result = $conn->query($sql);
+                $row = mysqli_fetch_assoc($result);
+                
+                while($row2= mysqli_fetch_assoc($result)){
+                    if($row2['next_form']==1){
+
+                    }
+                }
+                if($row['next_form']==1){
+                    ?>
+
+                    <div class="new-card-block">
+                        <div class="new-card">
+                            <div>
+                                <h1>Post Leave Updation</h1>
+                                <span>Kindly update the od completion document</span>
+                            </div>
+                            <div>
+                                <a href="leaveform.php" class="button">Open Form</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php
+                }
+
+                if (($result->num_rows == 0) ) {
+
+                ?>
+                    <div class="new-card-block">
+                        <div class="new-card">
+                            <div>
+                                <h1>Leave Form</h1>
+                                <span>Do you want to Apply Leave</span>
+                            </div>
+                            <div>
+                                <a href="leaveform.php" class="button">Apply Leave</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } else {
+                    //echo "varata mamea";
+                    //$row = mysqli_fetch_assoc($result);
+
+
+                ?>
+
+                    <div class="new-card-block">
+                        <div class="new-card">
+                            <div>
+                                <h1>Leave Status</h1>
+                                <span>The leave Form has been submitted</span>
+                                <a href="pending.php" class="button button-small">View Status</a>
+                            </div>
+
+                            <div class="containerB">
+                                <ul class="progressbar">
+                                    <li class="<?php echo getStatusClass($row['hod']); ?>" data-symbol="<?php echo getStatusSymbol($row['hod']); ?>">
+                                        HOD <span class="step-symbol"><?php echo getStatusClass($row['hod']); ?></span>
+                                    </li>
+                                    <li class="<?php echo getStatusClass($row['aqict']); ?>" data-symbol="<?php echo getStatusSymbol($row['aqict']); ?> ">
+                                        AQICT <span class="step-symbol"><?php echo getStatusClass($row['aqict']); ?></span>
+                                    </li>
+                                    <li class="<?php echo getStatusClass($row['principal']); ?>" data-symbol="<?php echo getStatusSymbol($row['principal']); ?> ">
+                                        PRINCIPAL <span class="step-symbol"><?php echo getStatusClass($row['principal']); ?></span>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+                           
+
+                        </div>
+
+                    </div>
+
+                <?php
+                }
+                ?>
+
             </main>
         </div>
     </body>
@@ -135,5 +220,28 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'staff') {
 <?php
 } else {
     header("location:../Login/home.php");
+}
+
+function getStatusSymbol($status)
+
+{
+    if ($status == 1) {
+        return '✔';
+    } elseif ($status == 0) {
+        return '❌';
+    } else {
+        return '!';
+    }
+}
+
+function getStatusClass($status)
+{
+    if ($status == 1) {
+        return 'approved';
+    } elseif ($status == 0) {
+        return 'declined';
+    } else {
+        return 'pending';
+    }
 }
 ?>
