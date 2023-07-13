f15b81276466a67b91d425851b87eeec6a580022
Hashwar07 <132980152+Hashwar07@users.noreply.github.com>
2023-07-13 21:11:23 +0530

    admin page

--- admin_page/forms.php
+++ admin_page/forms.php
@@ -11,7 +11,7 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'admin') {
     <title>SJC ADMIN</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="./assets/css/tailwind.output.css" />
-    <link rel="stylesheet" href="./assets/css/ApproveForm.css" />
+    <link rel="stylesheet" href="./assets/css/ApproveForm1.css" />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="./assets/js/init-alpine.js"></script>
   </head>
